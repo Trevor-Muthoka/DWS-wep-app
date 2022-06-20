37210d34fff5bbaf25dacac845466050d2526e26
Natalie Ngina Ndetei <natalie.ndetei@strathmore.edu>
2022-06-20 21:06:55 +0300

    Added my project

---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8" >
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>@yield('title',config('app.name'))</title>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 backdrop" data-navbar-on-scroll="data-navbar-on-scroll">
+    <div class="container"><a class="navbar-brand d-flex align-items-center fw-bolder fs-2 fst-italic" href="#">
+        <div class="text-info">D</div>
+        <div class="text-warning">W</div>
+      </a>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto pt-2 pt-lg-0">
+          <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="#">Home</a></li>
+          <li class="nav-item px-2"><a class="nav-link fw-medium" href="#features">Jobs</a></li>
+          <li class="nav-item px-2"><a class="nav-link fw-medium" href="#pricing">About</a></li>
+        </ul>
+        <form class="ps-lg-5">
+          <button class="btn btn-lg btn-primary rounded-pill bg-gradient order-0" type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
+  </nav>
